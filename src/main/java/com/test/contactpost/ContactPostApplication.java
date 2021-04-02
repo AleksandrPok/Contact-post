2336152f23e6a12a62e00eef12c9a10f70b4dc42
OleksandrPokotylenko <aln.nko.of@gmail.com>
2021-04-02 11:18:01 +0300

    first commit

---
+++ src/main/java/com/test/contactpost/ContactPostApplication.java
@@ -0,0 +1,13 @@
+package com.test.contactpost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactPostApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContactPostApplication.class, args);
+    }
+
+}
